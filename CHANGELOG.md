21d0d80f4b77ccde66d4dbd42f8ecf321a8a0871
Baruch Sterin <baruchs@gmail.com>
2018-02-23 05:35:09 -0800

    Bumped version to 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
 # Change Log
 
+## 0.1.1
+- Fixed bugs in README.md.
+- Added a working Parcel example.
+
 ## 0.1
 - Initial release
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name='lektor-npm-support',
     author=u'Baruch Sterin',
     author_email='lektor-npm-support@bsterin.com',
-    version='0.1',
+    version='0.1.1',
     url='http://github.com/sterin/lektor-npm-support',
     license='BSD',
     description="Adds support for using npm/yarn to build assets in Lektor",
